12a4181ab734b1b5badc6377189ba31d59eb421f
Andrés <andcarv89@gmail.com>
2020-06-27 10:43:33 -0400

    3 patern challenge

---
+++ patron3.rb
@@ -0,0 +1,14 @@
+num = ARGV[0].to_i
+
+num.times do |i|
+    if i % 6 == 0 || i % 6 == 1
+        print "*"
+    elsif i % 6 == 2 || i % 6 == 3
+        print "."
+    elsif i % 6 == 4 || i % 6 == 5
+        print "|" 
+    else
+        print "\n"
+    end
+end
+print "\n"
