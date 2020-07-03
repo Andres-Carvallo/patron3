7b5fd2a78c107955e7551961d972b0b4bd3857ac
Andrés <andcarv89@gmail.com>
2020-07-03 16:43:46 -0400

    patern changes

--- patron3.rb
+++ patron3.rb
@@ -2,9 +2,9 @@ num = ARGV[0].to_i
 
 num.times do |i|
     if i % 6 == 0 || i % 6 == 1
-        print "*"
-    elsif i % 6 == 2 || i % 6 == 3
         print "."
+    elsif i % 6 == 2 || i % 6 == 3
+        print "*"
     elsif i % 6 == 4 || i % 6 == 5
         print "|" 
     else
